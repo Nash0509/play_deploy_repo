a99ae0a9698166578b23fa6ca25d5f343061f8c0
nash0509 <nishantsinghworkshard@gmail.com>
2026-06-20 16:32:16 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const port = 2000;
+
+app.use(cors())
+
+app.get("/", (req, res) => {
+  return res.status(200).json({
+    dashboardContent: "Hello you! Welcome to the live dashboard.",
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
